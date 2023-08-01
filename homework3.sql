d83a93b2550d3b5c118b33509e225cf120cd6fb0
Dmytro Sainozhenko <126772772+Sainozhenko@users.noreply.github.com>
2023-08-01 23:29:26 +0200

    Update homework3.sql

--- homework3.sql
+++ homework3.sql
@@ -30,3 +30,39 @@ SELECT Price*1.15 as NewPrice,*
 FROM [Products]
 Join Categories ON Categories.CategoryID=Products.CategoryID
 WHERE Categories.CategoryID=4
+
+Доп.задача
+создать схему БД для "онлайн-игры: шахматы" с помощью инструмента онлайн-проектирования
+
+  Таблица "players":
+player_id (первичный ключ) - уникальный идентификатор игрока
+creation_date - дата и время создания профиля игрока
+pseudonym - псевдоним (никнейм) игрока
+is_blocked - статус блокировки 
+  
+  Таблица "matches" (партии):
+match_id (первичный ключ) - уникальный идентификатор игры
+player1_id (внешний ключ к players) - идентификатор первого игрока в игре
+player2_id (внешний ключ к players) - идентификатор второго игрока в игре
+start_time - дата и время начала игры
+end_time - дата и время окончания игры 
+winner_id (внешний ключ к players) - идентификатор победителя игры 
+  
+  Таблица "messages":
+message_id (первичный ключ) - уникальный идентификатор сообщения
+sender_id (внешний ключ к players) - идентификатор отправителя сообщения
+receiver_id (внешний ключ к players) - идентификатор получателя сообщения
+message_text - текст сообщения
+send_time - дата и время отправки сообщения
+
+  Таблица "chats":
+chat_id (первичный ключ) - уникальный идентификатор чата
+player1_id (внешний ключ к players) - идентификатор первого игрока в чате
+player2_id (внешний ключ к players) - идентификатор второго игрока в чате
+
+  Таблица "maps":
+map_id (первичный ключ) - уникальный идентификатор карты (игрового поля)
+map_name - название карты
+map_data - данные карты (например фон, тема)
+
+![Image alt](https://github.com/sainozhenko/bd_hw/raw/main/https://github.com/Sainozhenko/BD_HW/blob/main/Screenshot%202023-08-01%20231314.png/Screenshot 2023-08-01 231314.png)
